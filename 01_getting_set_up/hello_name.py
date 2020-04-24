ff4fa1e24d5432164f07a489ec3f7d678f23857d
charleygfield <charleygfield@gmail.com>
2020-04-24 17:06:50 -0500

    Update hello_name.py

--- 01_getting_set_up/hello_name.py
+++ 01_getting_set_up/hello_name.py
@@ -1,1 +1,3 @@
-# Your code here...
+user_name = input('Please type your name and hit ENTER: ')
+name_length = len(user_name)
+print('Hello,', user_name.strip().upper(), '!' ' Your name is', name_length, 'letters long.')
